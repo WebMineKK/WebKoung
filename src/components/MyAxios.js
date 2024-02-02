9f69d087eaa0ad823acfb0fa1d2f93c3aa85013f
4142_Luck <68369328+AnouluckCPX@users.noreply.github.com>
2024-02-03 01:33:05 +0700

    edit new structure code with vite

--- src/components/MyAxios.js
+++
@@ -1,4 +0,0 @@
-import axios from 'axios'
-export const NewAxios = axios.create({
-    baseURL: "http://172.28.26.146:12345/",
-})
--- src/components/chart/Myaxios.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios'
-const Myaxios = axios.create({
-    baseURL: "http://localhost:5190/api/",
-})
-
-export default Myaxios
